feb315f99e6ff81c7249b210ca19b597aa286d4a
IvanLagos <lagosivan090@gmail.com>
2025-12-08 00:28:29 -0300

    Api en Render para pizzas

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,17 @@
-const API_BASE_URL = "https://pizzeria-api-zfys.onrender.com";
+const API_URL = "https://pizzeria-api-zfys.onrender.com";
 
 export async function getPizzas() {
-  const res = await fetch(`${API_BASE_URL}/api/pizzas`);
+  try {
+    const res = await fetch(`${API_URL}/api/pizzas`);
 
-  if (!res.ok) {
-    throw new Error("Error al obtener las pizzas");
+    if (!res.ok) {
+      throw new Error("Error al obtener las pizzas");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error en getPizzas:", error);
+    throw error;
   }
-
-  const data = await res.json();
-  return data;
 }
